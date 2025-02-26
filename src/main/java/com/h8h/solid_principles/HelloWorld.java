c3667028fa5f14659ca31d93c3642ccb3b2a90fd
jakqui <jakqueline24@gmail.com>
2025-02-26 14:09:50 +0000

    Primer commit

---
+++ src/main/java/com/h8h/solid_principles/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.h8h.solid_principles;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ src/test/java/com/h8h/solid_principles/SolidPrinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h8h.solid_principles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidPrinciplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
